0c8ef3038368eab4c5d60031a8742c29c28ede44
Johannes Karhapää <johannes.karhapaa@aalto.fi>
2023-11-01 22:08:29 +0200

    add comments and nameguards

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,3 +1,10 @@
+/*
+ *Our own excellent vector class
+ */
+
+#ifndef _VECTOR_HPP_
+#define _VECTOR_HPP_
+
 #include <iostream>
 #include <cmath>
 
@@ -18,10 +25,16 @@ public:
 	    return Vector(x_-b.x_, y_-b.y_, z_-b.z_);
     }
 
+	/**
+	 * \brief Multiplication by scalar
+	 */
     Vector operator*(double k) {
 	    return Vector(k*x_, k*y_, k*z_);
     }
 
+	/**
+	 * \brief Returns length of the vector 
+	 */
     double Len() { 
 	    return std::sqrt(std::pow(x_, 2) + std::pow(y_, 2) + std::pow(z_, 2));
     } 
@@ -30,10 +43,16 @@ public:
 	    return *this * (1/Len());
     } 
 
+	/**
+	 * \brief Dot Product 
+	 */
     double operator*(Vector& v2) {
 	    return x_ * v2.x_ + y_ * v2.y_ + z_ * v2.z_;
     }
 
+	/**
+	 * \brief Cross Product 
+	 */
     Vector operator%(Vector& v2) {
 	    return Vector(y_*v2.z_ - z_*v2.y_,  z_*v2.x_ - x_*v2.z_,
 			    x_*v2.y_ - y_*v2.x_);
@@ -53,3 +72,5 @@ std::ostream& operator<<(std::ostream& os, const Vector& v) {
     os << '(' << v.x_ << ',' << v.y_ << ',' << v.z_  << ')';
     return os;
 }
+
+#endif //! _VECTOR_HPP_
