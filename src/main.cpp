6ea1bd036274ca5bda2e5fb4cbb46fd5ded79ae0
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-11-01 20:57:33 +0200

    Add SFML dependency and code to draw some pixels

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,44 @@
 */
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
+int main() {
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600), 32), "SFML Window");
 
-int main(){
+    // Create an image to work with
+    sf::Image image;
+    image.create(sf::Vector2u(800, 600), sf::Color::Transparent);
 
-    std::cout << "Path tracing" << std::endl;
+    int index = 0;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        // Modify specific pixels each frame
+        for (int x = 100; x < 100 + index; x++) {
+            for (int y = 100; y < 200; y++) {
+                image.setPixel(sf::Vector2u(x, y), sf::Color::Red);
+            }
+        }
+
+        // Load the image into a texture and display it
+        sf::Texture texture;
+        bool didLoad = texture.loadFromImage(image);
+        sf::Sprite sprite(texture);
+
+        window.clear();
+
+        // Draw the sprite with the colored pixels
+        window.draw(sprite);
+
+        window.display();
+        index++;
+    }
 
     return 0;
 }
