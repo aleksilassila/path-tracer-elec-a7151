b862e8b6b42bd03231deca8e14223fd3e62e61a2
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-11-04 17:51:02 +0200

    feat: Render simple flat-shaded sphere

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,48 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "utils/ray.hpp"
+
+sf::Color getPixelColor(float x, float y) {
+    // This code calculates if there are >= 1 intersections between a ray
+    // shot from cameraOrigin to a direction of (x, y, 1) (where x and y
+    // depend on current pixel coordinate and are values from -1 to 1)
+    // and a sphere with radius 0.5 and center at (0, 0, 0).
+
+    Vector cameraOrigin = Vector(0, 0, -1);
+    float sphereRadius = 0.5;
+
+    float xScaled = x * 2 - 1;
+    float yScaled = y * 2 - 1;
+
+    Ray ray = Ray(cameraOrigin, Vector(xScaled, yScaled, 1).Norm());
+    Vector rayDir = ray.GetDirection();
+    Vector rayOrig = ray.GetOrigin();
+
+    float a = rayDir * rayDir;
+    float b = 2 * (rayDir * rayOrig);
+    float c = (rayOrig * rayOrig) - sphereRadius * sphereRadius;
+
+    float discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) { // No hits
+        return sf::Color::Black;
+    }
+
+    return sf::Color::Magenta;
+}
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600), 32), "SFML Window");
+    unsigned int width = 600;
+    unsigned int height = 600;
+
+    sf::Vector2u windowSize(width, height);
+
+    sf::RenderWindow window(sf::VideoMode(windowSize, 32), "SFML Window");
 
     // Create an image to work with
     sf::Image image;
-    image.create(sf::Vector2u(800, 600), sf::Color::Transparent);
+    image.create(windowSize, sf::Color::Transparent);
 
     int index = 0;
     while (window.isOpen()) {
@@ -22,11 +57,18 @@ int main() {
         }
 
         // Modify specific pixels each frame
-        for (int x = 100; x < 100 + index; x++) {
-            for (int y = 100; y < 200; y++) {
-                image.setPixel(sf::Vector2u(x, y), sf::Color::Red);
+//        for (int x = 100; x < 100 + index; x++) {
+//            for (int y = 100; y < 200; y++) {
+//                image.setPixel(sf::Vector2u(x, y), sf::Color::Red);
+//            }
+//        }
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setPixel(sf::Vector2u(x, y), getPixelColor((float) x / width, (float) y / height));
             }
         }
+
 
         // Load the image into a texture and display it
         sf::Texture texture;
