389792cdbe62a4540e6a28be6d3812b91a4c1321
Johannes Karhapää <johannes.karhapaa@aalto.fi>
2023-11-26 00:20:35 +0200

    Add Parallelogram object

--- src/world/objects.cpp
+++ src/world/objects.cpp
@@ -83,6 +83,37 @@ double Object::Triangle::GetIntersectionDistance(const Ray &ray) {
     return t > 0 ? t : 0;
 }
 
+Object::Parallelogram::Parallelogram(const Vector &origin, const Vector &a, const Vector &b, const Material &material) :
+        Object(origin, material), a_(a), b_(b) {
+    origin_ = origin;
+    oa_ = a_ - origin_;
+    ob_ = b_ - origin_;
+    oa_ob_ = oa_ % ob_;
+    n_ = oa_ob_.Norm();
+}
+
+Vector Object::Parallelogram::Normal(const Vector &point) { return n_; }
+//This does actually not need a point, have to think about this...
+
+double Object::Parallelogram::GetIntersectionDistance(const Ray &ray) {
+    Vector dir = ray.GetDirection();
+    double det = dir * (oa_ob_);
+    //std::cout << "oa: " << oa_ << "ob: " << ob_ << "oa_ob: " << oa_ob_ << std::endl;
+    //std::cout << origin_ << std::endl;
+    if ((-0.0001 < det) && (det < 0.0001)) return 0;
+    //std::cout << "det: " << det << std::endl;
+    Vector or_or = ray.GetOrigin() - origin_;
+    Vector temp = or_or * (1 / det);
+    double u = ob_ % dir * temp;
+    //std::cout << "u: " << u << std::endl;
+    if (u < 0 || u > 1) return 0;
+    double v = dir % oa_ * temp;
+    //std::cout << "v: " << v << std::endl;
+    if (v < 0 || v > 1) return 0;
+    double t = -(oa_ob_ * temp);
+    //std::cout << "t: " << t << std::endl;
+    return t > 0 ? t : 0;
+}
 std::ostream &Object::operator<<(std::ostream &os, const Object &obj) {
     os << "origin: " << obj.origin_;
     return os;
--- src/world/objects.hpp
+++ src/world/objects.hpp
@@ -112,6 +112,39 @@ namespace Object {
 
     };
 
+    class Parallelogram : public Object {
+
+/*
+* Parallelogram class inherits from Object
+*/
+
+    private:
+
+        Vector origin_;
+        Vector a_;
+        Vector b_;
+        Vector oa_; // a_ - origin_
+        Vector ob_; // b_ - origin_
+        Vector oa_ob_; // o_a cross o_b
+        Vector n_;
+
+    public:
+
+        Parallelogram(const Vector &origin, const Vector &a, const Vector &b, const Material &material);
+
+        ~Parallelogram() override = default;
+
+        /*
+        * Calculates intersection of Ray and Parallelogram if it exists
+        */
+        double GetIntersectionDistance(const Ray &ray) override;
+
+        Vector Normal(const Vector &point) override;
+        /*
+        * Calculates normal vector of triangle at point given by vector point
+        */
+
+    };
 } // namespace Object
 
 #endif // OBJECTS_HPP
